2a7f5c0491c58cb6b18559602689227d63640c3c
UEMURA TAICHI <126448744+taichi0373@users.noreply.github.com>
2026-01-17 19:26:23 +0900

    Revert "DDL, DMLの修正"

--- config/database/setup.sql
+++ config/database/setup.sql
@@ -1,18 +1,19 @@
 -- 1. DDL
-\i '/docker-entrypoint-initdb.d/DDL/TABLE/NAT_AGENCY.SQL';
-\i '/docker-entrypoint-initdb.d/DDL/TABLE/NAT_BENEFIT_CATEGORY.SQL';
-\i '/docker-entrypoint-initdb.d/DDL/TABLE/NAT_MUNICIPALITY.SQL';
-\i '/docker-entrypoint-initdb.d/DDL/TABLE/NAT_BENEFIT.SQL';
-\i '/docker-entrypoint-initdb.d/DDL/TABLE/NAT_BENEFIT_ELIGIBILITY.SQL';
-\i '/docker-entrypoint-initdb.d/DDL/TABLE/NAT_FARE_DISCOUNT.SQL';
-\i '/docker-entrypoint-initdb.d/DDL/TABLE/NAT_COMMUNITY_BUS.SQL';
-\i '/docker-entrypoint-initdb.d/DDL/TABLE/NAT_USER.SQL';
+\i '/docker-entrypoint-initdb.d/DDL/TABLE/SPRING_SESSION.SQL';
+\i '/docker-entrypoint-initdb.d/DDL/TABLE/MUNICIPALITY.SQL';
+\i '/docker-entrypoint-initdb.d/DDL/TABLE/COMMUNITY_BUS.SQL';
+\i '/docker-entrypoint-initdb.d/DDL/TABLE/USERS.SQL';
+\i '/docker-entrypoint-initdb.d/DDL/TABLE/GTFS_BENEFITS.SQL';
+\i '/docker-entrypoint-initdb.d/DDL/TABLE/STORE_BENEFIT.SQL';
+\i '/docker-entrypoint-initdb.d/DDL/TABLE/STORE_CONDITION.SQL';
+\i '/docker-entrypoint-initdb.d/DDL/TABLE/BENEFITS.SQL';
+\i '/docker-entrypoint-initdb.d/DDL/TABLE/CONDITIONS.SQL';
 
 -- 2. DML
-\i '/docker-entrypoint-initdb.d/DML/TABLE/NAT_AGENCY.SQL';
-\i '/docker-entrypoint-initdb.d/DML/TABLE/NAT_BENEFIT_CATEGORY.SQL';
-\i '/docker-entrypoint-initdb.d/DML/TABLE/NAT_MUNICIPALITY.SQL';
-\i '/docker-entrypoint-initdb.d/DML/TABLE/NAT_BENEFIT.SQL';
-\i '/docker-entrypoint-initdb.d/DML/TABLE/NAT_BENEFIT_ELIGIBILITY.SQL';
-\i '/docker-entrypoint-initdb.d/DML/TABLE/NAT_FARE_DISCOUNT.SQL';
-\i '/docker-entrypoint-initdb.d/DML/TABLE/NAT_COMMUNITY_BUS.SQL';
+\i '/docker-entrypoint-initdb.d/DML/TABLE/MUNICIPALITY.SQL';
+\i '/docker-entrypoint-initdb.d/DML/TABLE/COMMUNITY_BUS.SQL';
+\i '/docker-entrypoint-initdb.d/DML/TABLE/GTFS_BENEFITS.SQL';
+\i '/docker-entrypoint-initdb.d/DML/TABLE/STORE_BENEFIT.SQL';
+\i '/docker-entrypoint-initdb.d/DML/TABLE/STORE_CONDITION.SQL';
+\i '/docker-entrypoint-initdb.d/DML/TABLE/BENEFITS.SQL';
+\i '/docker-entrypoint-initdb.d/DML/TABLE/CONDITIONS.SQL';
