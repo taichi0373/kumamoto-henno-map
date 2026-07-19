7909e7f8365a1b509049f0c4873db0d266460f1c
taichi0373 <ta2i0chi02@gmail.com>
2026-07-19 17:10:03 +0900

    fix: 現在地周辺ボタンをクリア・検索ボタンと同じ行に配置

--- apps/front/src/components/organisms/AppSearchBenefit.vue
+++ apps/front/src/components/organisms/AppSearchBenefit.vue
@@ -34,12 +34,10 @@
       <div class="form-btn">
         <AppButton :label="'クリア'" :primary="false" :icon="'pi pi-trash'" @click="clearConditions" />
         <AppButton type="submit" :label="'検索'" :primary="true" :icon="'pi pi-search'" :disabled="isLoading" />
-      </div>
-      <!-- 現在地周辺フィルターボタン -->
-      <div class="form-btn mt-2">
+        <!-- 現在地周辺フィルターボタン -->
         <AppButton
           label="現在地周辺"
-          severity="secondary"
+          :primary="false"
           icon="pi pi-map-marker"
           @click="filterByCurrentLocation"
         />
